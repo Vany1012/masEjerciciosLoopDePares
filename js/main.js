9f658a27df3eaf96aec4d5c933c0095f5d2c7429
Vany1012 <alvarezvillalvavanya@gmail.com>
2025-03-18 11:16:28 -0600

    Subiendo ajuste

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function loopDePares(numero){
     for (let i=0; i<=100; i++)
         if ((i+numero)%2 == 0){
             console.log(`El número ${numero+i} es par.`)
+        } else{
+            console.log(`${numero+i}`)
         }
 }
 loopDePares(5);
